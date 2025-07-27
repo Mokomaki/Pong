7a20ef0cd2e39c9923a0fe48e5082a7a791fdc0f
Risto Haila <grest.hyber@gmail.com>
2025-07-27 16:21:29 +0300

    [DP-15] Set up SFML

--- PongGame/Build-Pong.lua
+++ PongGame/Build-Pong.lua
@@ -1,7 +1,7 @@
 project "PongGame"
    kind "ConsoleApp"
    language "C++"
-   cppdialect "C++20"
+   cppdialect "C++17"
    targetdir "Binaries/%{cfg.buildcfg}"
    staticruntime "off"
 
@@ -14,30 +14,52 @@ project "PongGame"
         "Source/**.c"
     }
 
+
    includedirs
    {
       "Source",
+      "../Vendor/SFML/Include"
+   }
+
+   libdirs
+   {
+        "../Vendor/SFML/Lib"
    }
 
    links
    {
-      
+        "winmm.lib",
+        "opengl32.lib",
+        "gdi32.lib"
    }
+
 
    targetdir ("../Binaries/" .. OutputDir .. "/%{prj.name}")
    objdir ("../Binaries/Intermediates/" .. OutputDir .. "/%{prj.name}")
 
    filter "system:windows"
        systemversion "latest"
-       defines { "WINDOWS" }
+       defines { "WINDOWS", "SFML_STATIC"}
 
    filter "configurations:Debug"
-       defines { "DEBUG" }
+       defines { "DEBUG", "SFML_STATIC"}
        runtime "Debug"
        symbols "On"
+       links
+       {
+            "sfml-graphics-s-d.lib",
+            "sfml-system-s-d.lib",
+            "sfml-window-s-d.lib"
+       }
 
    filter "configurations:Release"
-       defines { "RELEASE" }
+       defines { "RELEASE", "SFML_STATIC"}
        runtime "Release"
        optimize "On"
        symbols "On"
+        links
+       {
+            "sfml-graphics-s.lib",
+            "sfml-system-s.lib",
+            "sfml-window-s.lib"
+       }
